295ff951de6489c905487d9a76f2e2fcaa38c971
Paulo Fagundes <paulohefagundes@gmail.com>
2017-08-02 20:19:55 -0600

    implement problem 476 - number complement

---
+++ tests/numberComplementTest.cpp
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+#include "numberComplement.cpp"
+
+TEST_CASE("numberComplement") {
+  // setup
+  Solution solution = Solution();
+
+  REQUIRE(solution.findComplement(5) == 2);
+  REQUIRE(solution.findComplement(2) == 1);
+  REQUIRE(solution.findComplement(1) == 0);
+  REQUIRE(solution.findComplement(0) == 1);
+}
