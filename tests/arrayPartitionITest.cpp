ab24aa2dab00bde4253584a5ba6e37fa401111a8
Paulo Fagundes <paulohefagundes@gmail.com>
2017-08-02 19:29:21 -0600

    implement problem 561 - array partition I in c++

---
+++ tests/arrayPartitionITest.cpp
@@ -0,0 +1,19 @@
+#include "catch.hpp"
+#include "arrayPartitionI.cpp"
+
+TEST_CASE("arrayPairSum") {
+  // setup
+  std::vector<int> list {1, 4, 3, 2};
+  Solution solution = Solution();
+
+  REQUIRE(solution.arrayPairSum(list) == 4);
+
+  list.clear();
+  list.push_back(7);
+  list.push_back(3);
+  list.push_back(1);
+  list.push_back(0);
+  list.push_back(0);
+  list.push_back(6);
+  REQUIRE(solution.arrayPairSum(list) == 7);
+}
