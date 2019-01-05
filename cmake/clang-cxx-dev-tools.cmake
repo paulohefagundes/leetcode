027ccf5f9544b807abe5fcdf33956d9649932c27
Paulo Fagundes <paulohefagundes@gmail.com>
2019-01-04 22:43:42 -0700

    make some CMake improvements

--- cmake/clang-cxx-dev-tools.cmake
+++ cmake/clang-cxx-dev-tools.cmake
@@ -1,17 +1,30 @@
 # Additional targets to perform clang-format/clang-tidy
-# Get all project files
-file(GLOB_RECURSE
-     ALL_CXX_SOURCE_FILES
-     *.[chi]pp *.[chi]xx *.cc *.hh *.ii *.[CHI]
-     )
 
-# remove the catch.hpp from the list -> we don't want to clang-format it
-list(REMOVE_ITEM ALL_CXX_SOURCE_FILES "${CMAKE_CURRENT_SOURCE_DIR}/tests/include/catch.hpp")
+# clang-tidy
+find_program(CLANG_TIDY_EXE
+  NAMES "clang-tidy"
+  DOC "Path to clang-tidy executable")
+if(CLANG_TIDY_EXE)
+  set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_EXE}")
+endif()
 
-# Adding clang-format target if executable is found
+# clang-format
 find_program(CLANG_FORMAT "clang-format")
 if(CLANG_FORMAT)
-  # this custom target can be invoked with `make clang-format`
+  # make a list of all source files
+  file(GLOB_RECURSE
+    ALL_CXX_SOURCE_FILES
+    *.[ch]pp *.[ch]xx *.cc *.hh *.[ch])
+
+  # remove all of the third-party/* files from the list
+  foreach(SOURCE_FILE ${ALL_CXX_SOURCE_FILES})
+    string(FIND ${SOURCE_FILE} third-party THIRD_PARTY_DIR_FOUND)
+    if(NOT ${THIRD_PARTY_DIR_FOUND} EQUAL -1)
+      list(REMOVE_ITEM ALL_CXX_SOURCE_FILES ${SOURCE_FILE})
+    endif()
+  endforeach()
+
+  # add the clang-format custom target
   add_custom_target(
     clang-format
     COMMAND /usr/bin/clang-format
